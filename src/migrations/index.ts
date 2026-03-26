6e41524e685e94df9fec054a008315c418aaceae
zenzty <hdtoki@gmail.com>
2026-03-26 21:22:57 +0100

    chore: add incremental migration snapshot for dev database alignment
    Captures current schema state so Vercel preview builds can run payload migrate without the dev-mode drift error.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
-import * as migration_20260322_184027 from './20260322_184027'
-import * as migration_20260324_image_optimizer from './20260324_image_optimizer'
-import * as migration_20260325_remove_image_optimizer from './20260325_remove_image_optimizer'
+import * as migration_20260322_184027 from './20260322_184027';
+import * as migration_20260324_image_optimizer from './20260324_image_optimizer';
+import * as migration_20260325_remove_image_optimizer from './20260325_remove_image_optimizer';
+import * as migration_20260326_202206 from './20260326_202206';
 
 export const migrations = [
   {
@@ -18,4 +19,9 @@ export const migrations = [
     down: migration_20260325_remove_image_optimizer.down,
     name: '20260325_remove_image_optimizer',
   },
-]
+  {
+    up: migration_20260326_202206.up,
+    down: migration_20260326_202206.down,
+    name: '20260326_202206'
+  },
+];
